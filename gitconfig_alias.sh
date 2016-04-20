92839ace9053b19f1ec64a2a1f690baf7ecd223e
Igor Bodnar <ibodnar@artlebedev.ru>
2016-04-20 12:33:02 +0300

    Git aliases GIt status Removes orig files

---
+++ gitconfig_alias.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git config --global alias.st "status" # short git st instead of git status
+git config --global alias.cleanorig "clean -f -X *.orig" # git cleanorig removes all .orig files after merge
+
